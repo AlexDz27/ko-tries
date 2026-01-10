3ac9f7808757cdb035a8c7b273880b1cc151593d
Alex Dzyuba <alexeydzyuba27@gmail.com>
2026-01-10 23:57:33 +0300

    w

---
+++ scratch/try-12/vue-ex/src/components/Number.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed, watch } from 'vue'
+
+const { number } = defineProps({
+  number: {
+    type: Number,
+  }
+})
+const emit = defineEmits(['onTwelve'])
+
+watch(() => number, async (oldVal, newVal) => {
+  if (oldVal === 12) {
+    emit('onTwelve')
+    alert('got 12')
+  }
+})
+
+const fontSize = computed(() => Math.max(number, 10))
+</script>
+
+<template>
+  <p>
+    <b :style="{fontSize: `${fontSize}px`}">{{ number }}</b>
+  </p>
+</template>
